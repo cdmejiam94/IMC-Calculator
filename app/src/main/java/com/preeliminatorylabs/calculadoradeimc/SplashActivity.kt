3f44beb8ef76bc68fe33c71c51a48c9330e1eeaf
Carlos Mejia <cdmejiam@gmail.com>
2020-02-21 19:17:21 -0600

    Splash Screen Implemented

--- app/src/main/java/com/preeliminatorylabs/calculadoradeimc/SplashActivity.kt
+++ app/src/main/java/com/preeliminatorylabs/calculadoradeimc/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.preeliminatorylabs.calculadoradeimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.preeliminatorylabs.calculadoradeimc.view.activity.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_TIME_OUT: Long = 1500
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },SPLASH_TIME_OUT)
     }
 }
